82d1c1959fc24279435ebd649be5bf0713eddc3c
ANAM FAHEEM KHAN <127435454+Anam-Fkhan@users.noreply.github.com>
2024-08-28 15:23:44 +0530

    Add files via upload

---
+++ Src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "812c2a34b4msh397c74d89685b64p16a462jsnc59ee7070ec7",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5786d3ad8dc15c3a539411cc1595a5eb"; // enter your key from openweather API
